1d7084bd45a2690bffe51b5e344700e50f358c35
Jalal Ahmad <jahmad@fareportal.com>
2013-08-06 18:34:23 +0530

    Added status metrics

--- newrelic_pmta_agent.rb
+++ newrelic_pmta_agent.rb
@@ -14,8 +14,29 @@ module PmtaAgent
     agent_config_options :hertz  # frequency of the periodic functions
     agent_human_labels("PowerMTA Agent") { "PowerMTA statistics" }
 
+    ROOT_URL = :SERVER_ROOT || "http://localhost:8080"
+    QUEUE_URL = "/queues?format=xml"
+    STATUS_URL = "/status?format=xml"
+    @agent = Mechanize.new
+
+
     def poll_cycle
-      
+      stats = status
+      report_metric "Component/Status/Messages/In[msgs]" , stats.in.msg.text()
+      report_metric "Component/Status/Messages/Out[msgs]" , stats.out.msg.text()
+      report_metric "Component/Status/Recepients/In[rcpts]" , stats.in.rcp.text()
+      report_metric "Component/Status/Recepients/Out[rcpts]" , stats.out.rcp.text()
+      report_metric "Component/Status/Bytes/In[kb]" , stats.in.kb.text()
+      report_metric "Component/Status/Bytes/Out[kb]" , stats.out.kb.text()
+    end
+
+    def queues
+      response = @agent.get ROOT_URL + QUEUE_URL
+
+    end
+    def status
+      response = @agent.get ROOT_URL + STATUS_URL
+      response.search('/rsp/data/status/traffic/lastMin')
     end
 
   end
